1ce3e16bc37b5741b985d4a585d4baa8efd0618b
Gautam Hathi <gautamhathi@live.com>
2018-02-10 16:16:08 -0800

    adding user agent to request

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 import urllib.request, json 
 
+req = urllib.request.Request(
+    url, 
+    data=None, 
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+    }
+)
+
 url = "https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=47.449474&lng=-122.309912&fDstL=0&fDstU=50"
-with urllib.request.urlopen(url) as page:
+with urllib.request.urlopen(req) as page:
     data = json.loads(page.read().decode())
     print(data)
