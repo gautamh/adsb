f4c583eb50bb86e18bc9572f8d3cbdd0294758c8
Gautam Hathi <gautamhathi@live.com>
2018-03-02 23:04:48 -0800

    organizing plot_tracts into functions for use in other files

--- plot_tracts.py
+++ plot_tracts.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import shapely
 
+
 def generate_viewing_triangles(x1, y1, x2, y2, offset_dist):
     segment = shapely.geometry.LineString(coordinates=[(x1, y1), (x2, y2)])
     left = segment.parallel_offset(offset_dist, 'left')
@@ -20,17 +21,36 @@ def generate_viewing_triangles(x1, y1, x2, y2, offset_dist):
     right_triangle = shapely.geometry.polygon.Polygon(list(right_scaled.coords) + [((x2+x1)/2,(y2+y1)/2)])
     return [left_triangle, right_triangle]
 
-# print(shapely.geometry.box(-77.7, 38.5, -76.2, 39.2))
-studyareas = generate_viewing_triangles(-77.06, 38.89, -77.05, 38.88, 0.1)
-print(studyareas)
-tracts = gpd.GeoDataFrame.from_file("/mnt/c/Users/Gautam/Downloads/Profile-County_Tract/Profile-County_Tract.gdb", layer='Tract_2010Census_DP1')
-tracts['popdensity'] = tracts['DP0010001']/tracts['ALAND10']
 
-wash_tracts = tracts[tracts.geometry.intersects(studyareas[0]) | tracts.geometry.intersects(studyareas[1])]
+def load_tracts():
+    tracts = gpd.GeoDataFrame.from_file("/mnt/c/Users/Gautam/Downloads/Profile-County_Tract/Profile-County_Tract.gdb", layer='Tract_2010Census_DP1')
+    tracts['popdensity'] = tracts['DP0010001']/tracts['ALAND10']
+    return tracts
 
-#plt.hist(tracts['popdensity'], bins=25)
-tractplot = wash_tracts.plot(column='popdensity')
-polys1 = gpd.GeoSeries(studyareas)
-df1 = gpd.GeoDataFrame({'geometry': polys1, 'df1':[1, 2]})
-df1.plot(ax=tractplot, color='green', alpha=0.5)
-plt.show()
+
+def get_triangle_tract_intersection(tracts, studyareas):
+    print(studyareas)
+    intersect_tracts = tracts[tracts.geometry.intersects(studyareas[0]) | tracts.geometry.intersects(studyareas[1])]
+    left_pop = np.sum(intersect_tracts[intersect_tracts.geometry.intersects(studyareas[0])]['DP0010001'])
+    right_pop = np.sum(intersect_tracts[intersect_tracts.geometry.intersects(studyareas[1])]['DP0010001'])
+    print("Left pop: {}".format(left_pop))
+    print("Right pop: {}".format(right_pop))
+    return intersect_tracts
+
+
+def plot_tracts_and_triangles(tracts, studyareas):
+    tractplot = tracts.plot(column='popdensity')
+    polys1 = gpd.GeoSeries(studyareas)
+    df1 = gpd.GeoDataFrame({'geometry': polys1, 'df1':[0] * len(studyareas)})
+    df1.plot(ax=tractplot, color='green', alpha=0.5)
+    plt.show()
+
+
+def run_dca_plot():
+    tracts = load_tracts()
+    studyareas = generate_viewing_triangles(-77.06, 38.89, -77.05, 38.88, 0.1)
+    intersect_tracts = get_triangle_tract_intersection(tracts, studyareas)
+    plot_tracts_and_triangles(intersect_tracts, studyareas)
+
+
+#run_dca_plot()
