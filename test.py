f44ee6cca41548a17c0c98786d91133052edcecb
Gautam Hathi <gautamhathi@live.com>
2018-03-11 22:49:06 -0700

    cleaning up scraper to prep for maybe scraping multiple airports

--- test.py
+++ test.py
@@ -2,55 +2,54 @@ import urllib.request, json
 import time
 from google.cloud import datastore
 
-# KSEA lat and long
-airport_lat = 47.449474
-airport_long = -122.309912
-
-# Radius from airport (in mi?) to track flights
-radius_from_airport = 20
-
-url = "https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat={}&lng={}&fDstL=0&fDstU={}".format(airport_lat, airport_long, radius_from_airport)
-req = urllib.request.Request(
-    url, 
-    data=None, 
-    headers={
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-    }
-)
-
-samples = 30
-delay = 45
-
 # Instantiates a client
 datastore_client = datastore.Client()
 
 # The kind for the new entity
-kind = 'FlightPoint'
+KIND = 'FlightPoint'
 
-for i in range(samples):
-    data = []
+# KSEA lat and long
+KSEA_lat = 47.449474
+KSEA_long = -122.309912
 
-    with urllib.request.urlopen(req) as page:
-        data = json.loads(page.read().decode())
+# Radius from airport (in mi?) to track flights
+radius_from_KSEA = 20
 
-    print(len(data['acList']))
-    for flight in data['acList']:
-        # The name/ID for the new entity
-        name = str(flight['Id']) + '-' + str(flight['PosTime'])
-        # The Cloud Datastore key for the new entity
-        flight_point_key = datastore_client.key(kind, name)
-        # Create the entity
-        flight_point = datastore.Entity(key=flight_point_key)
-        for k,v in flight.items():
-            flight_point[k] = v
+samples = 30
+delay = 45
+
+URL_TEMPLATE = 'https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat={}&lng={}&fDstL=0&fDstU={}'
+USER_AGENT_STR = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+
+def sample_flight_points(num_samples, delay, radius, lat, long, client):
+    url = URL_TEMPLATE.format(lat, long, radius)
+    req = urllib.request.Request(
+        url, 
+        data=None, 
+        headers={
+            'User-Agent': USER_AGENT_STR
+        }
+    )
+
+    for i in range(samples):
+        data = []
+
+        with urllib.request.urlopen(req) as page:
+            data = json.loads(page.read().decode())
+
+        print(len(data['acList']))
+        for flight in data['acList']:
+            # The name/ID for the new entity
+            name = str(flight['Id']) + '-' + str(flight['PosTime'])
+            # The Cloud Datastore key for the new entity
+            flight_point_key = datastore_client.key(KIND, name)
+            # Create the entity
+            flight_point = datastore.Entity(key=flight_point_key)
+            for k,v in flight.items():
+                flight_point[k] = v
         
-        datastore_client.put(flight_point)
+            client.put(flight_point)
 
-    #query = datastore_client.query(kind='FlightPoint')
-    #query.add_filter('To', '=', "KSEA Seattle Tacoma, United States")
+        time.sleep(delay)
 
-    #query_iter = query.fetch()
-    #for entity in query_iter:
-        #pass
-        #print(entity)
-    time.sleep(delay)
+sample_flight_points(samples, delay, radius_from_KSEA, KSEA_lat, KSEA_long, datastore_client)
