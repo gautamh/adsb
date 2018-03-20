46f18df4d1e950593599a624dc930f93c2eddd2a
Gautam Hathi <gautamhathi@live.com>
2018-03-19 23:17:16 -0700

    adding KDCA to scrape_flights

--- scrape_flights.py
+++ scrape_flights.py
@@ -21,8 +21,12 @@ KIND = 'FlightPoint'
 KSEA_lat = 47.449474
 KSEA_long = -122.309912
 
+KDCA_lat = 38.85120
+KDCA_long = -77.03774
+
 airport_coords = [
-    (KSEA_lat, KSEA_long)
+    #(KSEA_lat, KSEA_long)
+    (KDCA_lat, KDCA_long)
 ]
 
 
@@ -34,6 +38,17 @@ delay = 45
 
 URL_TEMPLATE = 'https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat={}&lng={}&fDstL=0&fDstU={}'
 USER_AGENT_STR = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+
+VALID_TO = [
+    'KDCA Ronald Reagan Washington National, United States',
+    'KSEA Seattle Tacoma, United States'
+]
+
+def should_record_flight(flightinfo, valid_to=None):
+    if (valid_to is not None and flightinfo['To'] not in valid_to):
+        return False
+
+    return True
 
 def sample_flight_points(num_samples, delay, radius, lat, long, client):
     url = URL_TEMPLATE.format(lat, long, radius)
@@ -53,6 +68,8 @@ def sample_flight_points(num_samples, delay, radius, lat, long, client):
 
         print(len(data['acList']))
         for flight in data['acList']:
+            if not should_record_flight(flight, VALID_TO):
+                continue
             # The name/ID for the new entity
             name = str(flight['Id']) + '-' + str(flight['PosTime'])
             # The Cloud Datastore key for the new entity
